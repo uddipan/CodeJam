9e1625566c193b762c55f5fe1d88706874078e4d
uddipan <uddipanm@gmail.com>
2015-01-11 19:44:43 -0800

    surce code

--- Qual_africa_2010/T9 Spelling/T9 Spelling/main.cpp
+++ Qual_africa_2010/T9 Spelling/T9 Spelling/main.cpp
@@ -15,41 +15,115 @@
 #include <queue>
 
 // MACRO for selecting the small/large input. Comment out for large input.
-#define SmallLargeInput
+//#define SmallLargeInput
 
 
 using namespace std;
 
 string GenericName = "/GitHub/CodeJam";
 
-string TestName = "/Qual_africa_2010/Reverse Words/Reverse Words/";
+string TestName = "/Qual_africa_2010/T9 Spelling/T9 Spelling/";
+
+void GetSeq(string &line, string &seq)
+{
+    char temp = NULL;
+    for (unsigned u=0; u < line.size(); u++)
+    {
+        if (line[u] == ' ')
+        {
+            if (temp == '0')
+            {
+                seq.push_back(' ');
+            }
+            seq.push_back('0');
+            temp = '0';
+        }
+        else if (line[u] < 'p')
+        {
+            int tmp = int(line[u] -'a');
+            char c = char(50 + tmp/3);
+            if (temp == c)
+            {
+                seq.push_back(' ');
+            }
+            
+            for (int i=0; i<=(tmp%3); i++)
+            {
+                seq.push_back(c);
+            }
+            temp = c;
+        }
+        else if (line[u] < 't')
+        {
+            if (temp == '7')
+            {
+                seq.push_back(' ');
+            }
+            int tmp = int(line[u] -'p');
+            for (int i=0; i<=(tmp%4); i++)
+            {
+                seq.push_back('7');
+            }
+            temp = '7';
+        }
+        else if (line[u] < 'w')
+        {
+            if (temp == '8')
+            {
+                seq.push_back(' ');
+            }
+            int tmp = int(line[u] -'t');
+            for (int i=0; i<=(tmp%3); i++)
+            {
+                seq.push_back('8');
+            }
+            temp = '8';
+        }
+        else if (line[u] <= 'z')
+        {
+            if (temp == '9')
+            {
+                seq.push_back(' ');
+            }
+            int tmp = int(line[u] -'w');
+            for (int i=0; i<=(tmp%4); i++)
+            {
+                seq.push_back('9');
+            }
+            temp = '9';
+        }
+        
+    }
+}
 
 int main(int argc, const char * argv[]) {
     string InFileName, OutFileName;
     
 #ifdef SmallLargeInput
-    InFileName = GenericName + TestName + "A-small-practice.in";
+    InFileName = GenericName + TestName + "C-small-practice.in";
     OutFileName = GenericName + TestName + "output-small.out";
 #else
-    InFileName = GenericName + TestName + "A-large-practice.in";
+    InFileName = GenericName + TestName + "C-large-practice.in";
     OutFileName = GenericName + TestName + "output-large.out";
 #endif
     
-    
-    string line;
+    int N;
+    string line, Seq;
     ifstream infile ( InFileName );
     ofstream outfile ( OutFileName );
     
     if (infile.is_open())
     {
-        infile >> line;
+        getline(infile,line);
+        N = atoi(line.c_str());
         
-        
-        if (outfile.is_open())
+        for(int i=0; i < N; i++)
         {
-            //outfile << "Case #" << i+1 << ": " << Item1 << " " << Item2 <<"\n";
+            getline(infile, line);
+            GetSeq(line, Seq);
+            outfile << "Case #" << i+1 << ": " << Seq <<"\n";
+            Seq.clear();
         }
-        
         
         infile.close();
         outfile.close();
