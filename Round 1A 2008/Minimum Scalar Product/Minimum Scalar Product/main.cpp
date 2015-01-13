a88df4722870b1f55b2484b8678128d6b859387e
uddipan <uddipanm@gmail.com>
2015-01-12 18:49:52 -0800

    2008 1A

---
+++ Round 1A 2008/Minimum Scalar Product/Minimum Scalar Product/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Minimum Scalar Product
+//
+//  Created by uddipan mukherjee on 1/12/15.
+//  Copyright (c) 2015 uddipan mukherjee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
